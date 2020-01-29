eb31b373483d9482d55d7209c08eca7086f188a1
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-29 21:54:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,12 +9,8 @@ class Shoe
   def initialize(brand)
     @brand = brand
     if BRANDS.include?(brand)
-    
-  end
-  
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
+    else
+      BRANDS << brand
   end
 
   def cobble
